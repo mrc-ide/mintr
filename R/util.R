70019155ec2c71742135bec61e3a9d16b1f17a40
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-04-20 19:29:51 +0100

    Utilities for working with package files

--- R/util.R
+++ R/util.R
@@ -1,3 +1,21 @@
 `%||%` <- function(a, b) { # nolint
   if (is.null(a)) b else a
 }
+
+
+mintr_path <- function(path) {
+  system.file(path, package = "mintr", mustWork = TRUE)
+}
+
+
+## TODO: This will be removed once RESIDE-121 is fixed
+schema_root <- function() {
+  mintr_path("schema")
+}
+
+
+read_json <- function(path) {
+  str <- paste(readLines(path), collapse = "\n")
+  class(str) <- "json"
+  str
+}
