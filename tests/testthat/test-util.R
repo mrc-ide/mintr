67d23108de06b5233a04fb7f34f34898affd358f
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-10-09 19:00:50 +0100

    Start implementation

--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -6,3 +6,18 @@ test_that("null-or-value works", {
   expect_equal(NULL %||% NULL, NULL)
   expect_equal(NULL %||% 2, 2)
 })
+
+
+test_that("assert_setequal gives useful messages", {
+  x <- rep(c("a", "b", "c"), each = 2)
+  expect_silent(assert_setequal(x, c("a", "b", "c")))
+  expect_error(assert_setequal(x, c("a", "b")),
+               "Invalid value for 'x':.*- Unexpected: c")
+  expect_error(assert_setequal(x, c("a")),
+               "Invalid value for 'x':.*- Unexpected: b, c")
+  expect_error(
+    assert_setequal(x, c("a", "d")),
+    "Invalid value for 'x':.*- Unexpected: b, c.*- Missing: d")
+  expect_error(assert_setequal(x, c("a", "b", "c", "d")),
+               "Invalid value for 'x':.*- Missing: d")
+})
