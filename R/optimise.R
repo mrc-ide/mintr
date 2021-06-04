e6f0994961ceebd68073d1cb93273e28a9830813
Mark Woodbridge <m.woodbridge@imperial.ac.uk>
2021-06-04 11:00:54 +0100

    Implement optimiser to strategize across regions

---
+++ R/optimise.R
@@ -0,0 +1,44 @@
+library(dplyr, warn.conflicts = FALSE)
+
+utils::globalVariables(c("zone", "intervention", "total_costs", "total_cases_averted", "value", "i", "j"))
+
+#' @import dplyr
+do_optimise <- function(data, budget) {
+  cost_df <- distinct(data, zone, intervention, total_costs) %>%
+    group_by(intervention) %>%
+    mutate(j = cur_group_id()) %>%
+    group_by(zone) %>%
+    mutate(i = cur_group_id()) %>%
+    ungroup()
+  cases_av_df <- distinct(data, zone, intervention, total_cases_averted) %>%
+    group_by(intervention) %>%
+    mutate(j = cur_group_id()) %>%
+    group_by(zone) %>%
+    mutate(i = cur_group_id()) %>%
+    ungroup()
+  cost <- function(zone, intervention) {
+    filter(cost_df, i == !!zone, j == !!intervention)$total_costs
+  }
+  cases_av <- function(zone, intervention) {
+    filter(cases_av_df, i == !!zone, j == !!intervention)$total_cases_averted
+  }
+  n_zones <- n_distinct(data$zone)
+  n_interventions <- n_distinct(data$intervention)
+
+  optimise <- function(budget) {
+    model <- rmpk::optimization_model(rmpk::ROI_optimizer("glpk"))
+    y <- model$add_variable("y", i = 1:n_zones, j = 1:n_interventions, type = "integer", lb = 0, ub = 1)
+    model$set_objective(rmpk::sum_expr(y[i, j] * cases_av(i, j), i = 1:n_zones, j = 1:n_interventions), sense = "max")
+    model$add_constraint(rmpk::sum_expr(y[i, j] * cost(i, j), i = 1:n_zones, j = 1:n_interventions) <= budget)
+    model$add_constraint(rmpk::sum_expr(y[i, j], j = 1:n_interventions) == 1, i = 1:n_zones)
+    model$optimize()
+    model$get_variable_value(y[i, j]) %>%
+      filter(value == 1) %>%
+      left_join(cost_df, c("i", "j")) %>%
+      left_join(select(cases_av_df, total_cases_averted, i, j), by = c("i", "j"))
+  }
+
+  optimise(budget) %>%
+    select(zone, intervention, total_costs, total_cases_averted) %>%
+    arrange(zone)
+}
