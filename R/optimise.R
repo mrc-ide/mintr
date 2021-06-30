aa26c29d5455b3818dba594cb71d200da9686195
Mark Woodbridge <m.woodbridge@imperial.ac.uk>
2021-06-30 15:35:13 +0100

    Simplify workaround following new release of ROIoptimizer

--- R/optimise.R
+++ R/optimise.R
@@ -2,19 +2,6 @@
 #' @import tidyr
 do_optimise <- function(data, budget) {
   zone <- intervention <- cost <- cases_averted <- i <- j <- NULL # used by dplyr
-
-  # Due to a limitation in the optimiser it currently requires at least two
-  # scenarios with non-zero cases_averted - this workaround therefore adds two
-  # dummy scenarios, with budget-exceeding costs to ensure they are not selected
-  data <- bind_rows(data,
-      distinct(data, zone) %>%
-        crossing(data.frame(
-          intervention = c("dummy_1", "dummy_2"),
-          cost = rep(budget * 10 + 1, 2),
-          cases_averted = rep(1, 2)
-        ))
-  )
-
   cost_df <- distinct(data, zone, intervention, cost) %>%
     group_by(intervention) %>%
     mutate(j = cur_group_id()) %>%
@@ -42,7 +29,11 @@ do_optimise <- function(data, budget) {
     model <- rmpk::optimization_model(rmpk::ROI_optimizer("glpk"))
     y <- model$add_variable("y", i = 1:n_zones, j = 1:n_interventions, type = "integer", lb = 0, ub = 1)
     model$set_objective(rmpk::sum_expr(y[i, j] * cases_av(i, j), i = 1:n_zones, j = 1:n_interventions), sense = "max")
-    model$add_constraint(rmpk::sum_expr(y[i, j] * cost(i, j), i = 1:n_zones, j = 1:n_interventions) <= budget)
+    # rmpk currently fails if all scenarios have zero cost. work around this by
+    # only constraining costs if some non-zero cost scenarios are provided.
+    if (n_interventions > 1) {
+      model$add_constraint(rmpk::sum_expr(y[i, j] * cost(i, j), i = 1:n_zones, j = 1:n_interventions) <= budget)
+    }
     model$add_constraint(rmpk::sum_expr(y[i, j], j = 1:n_interventions) == 1, i = 1:n_zones)
     model$optimize()
     model$get_variable_value(y[i, j]) %>%
