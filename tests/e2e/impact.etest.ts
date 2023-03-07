0021a608ca4a58451f3d2a9ef4e031d8204d84a1
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2023-03-07 11:20:30 +0000

    test failure

--- tests/e2e/impact.etest.ts
+++ tests/e2e/impact.etest.ts
@@ -3,3 +3,7 @@ import { test, expect } from '@playwright/test';
 test('example', async ({ page }) => {
   await page.locator(".navbar");
 });
+
+test('test nonexistent', async ({ page }) => {
+  await page.locator(".doesnotexist");
+});
