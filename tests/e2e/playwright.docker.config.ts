bcd746d4a8b11ba7dd58a616f01dc6aeff2b354c
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2023-03-08 13:40:51 +0000

    single worker

--- tests/e2e/playwright.docker.config.ts
+++ tests/e2e/playwright.docker.config.ts
@@ -6,8 +6,9 @@ const config: PlaywrightTestConfig = {
         baseURL: "http://mint:8080",
         screenshot: "only-on-failure"
     },
+    workers: 1,
     retries: 1,
-    timeout: 60000
+    timeout: 120000
 };
 
 export default config;
