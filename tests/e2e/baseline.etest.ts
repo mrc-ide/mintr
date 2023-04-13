b57bba613e70567dfa45a8cc8f7f0ea3ebfb9e95
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2023-04-13 17:36:45 +0100

    e2e test options

--- tests/e2e/baseline.etest.ts
+++ tests/e2e/baseline.etest.ts
@@ -18,3 +18,19 @@ test("expected baseline options exist", async ({page}) => {
     await expectOptionLabelAndName(rows.nth(7), "ITN population usage in last survey (%)", "itnUsage");
     await expectOptionLabelAndName(rows.nth(8), "IRS population coverage in last survey (%)", "itnUsage");
 });
+
+test("expected past ITN usage options exist", async({page}) => {
+    const options = await page.locator("select[name='itnUsage'] option");
+    await expectOptionLabelAndName(options.nth(0), "0% usage", "0%");
+    await expectOptionLabelAndName(options.nth(1), "20% usage", "20%");
+    await expectOptionLabelAndName(options.nth(2), "40% usage", "40%");
+    await expectOptionLabelAndName(options.nth(3), "60% usage", "60%");
+    await expectOptionLabelAndName(options.nth(4), "80% usage", "80%");
+});
+
+test("expected past IRS coverage options exist", async ({page}) => {
+    const options = await page.locator("select[name='sprayInput'] option");
+    await expectOptionLabelAndName(options.nth(0), "0% coverage", "0%");
+    await expectOptionLabelAndName(options.nth(1), "20% coverage", "60%");
+    await expectOptionLabelAndName(options.nth(2), "40% coverage", "80%");
+});
--- tests/e2e/helpers.ts
+++ tests/e2e/helpers.ts
@@ -93,6 +93,13 @@ export const expectOptionLabelAndName = async (optionRow, expectedLabel, expecte
     await expect(name).toBe(expectedName)
 };
 
+export const expectSelectOptionLabelAndValue = async (option, expectedLabel, expectedValue) => {
+    const label = await option.innerText();
+    await expect(label).toBe(expectedLabel);
+    const value = option.getAttribute("value");
+    await expect(value).toBe(expectedValue);
+};
+
 export const expectPlotDataSummarySeries = async (summary, expectedId, expectedName, expectedType, expectedCount,
                                                   expectedXFirst, expectedXLast, expectedYMin, expectedYMax, yTolerance = null) => {
     await expect(await summary.getAttribute("name")).toBe(expectedName);
