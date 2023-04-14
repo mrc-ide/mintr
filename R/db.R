6ddf098bbb7c148ec612d49ded7189a138e35037
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2023-04-14 17:53:10 +0100

    try that again

--- R/db.R
+++ R/db.R
@@ -68,8 +68,8 @@ mintr_db <- R6::R6Class(
         ret[[v]] <- round(ret[[v]] * options$population)
       }
       table <- mintr_db_transform_metabolic(ret, options$metabolic)
-      table[order(table$intervention),]
-      mintr_db_set_not_applicable_values(table)
+      sorted <- table[order(table$intervention),]
+      mintr_db_set_not_applicable_values(sorted)
     }
   ))
 
