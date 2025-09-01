5f18f19d3da408a5381c938c47ac25c401badf55
anmol thapar <mr.anmolthapar@gmail.com>
2025-09-01 09:57:44 +0100

    Refactor run_mint_scenarios call in run_emulator function; update Dockerfile to install Python 3.12

--- R/emulator.R
+++ R/emulator.R
@@ -1,9 +1,9 @@
-# TODO: update naming and make clearer.
+library(MINTer) # TODO: test and remove if needed
 run_emulator <- function(options) {
     transformed_options <- transform_options(options)
     minter_params <- build_minter_params(transformed_options)
 
-    results <- do.call(MINTer::run_mint_scenarios, minter_params)
+    results <- do.call(run_mint_scenarios, minter_params)
     # post-process results
 
     post_process_results(results)
