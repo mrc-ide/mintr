4b00f52a1e22a0ff0df218da678732291e573f7f
anmol thapar <mr.anmolthapar@gmail.com>
2025-09-25 11:04:55 +0100

    Refactor strategise and optimise functions to use 'region' instead of 'zone'; update related schemas and tests for consistency

--- R/strategise.R
+++ R/strategise.R
@@ -1,91 +1,32 @@
-get_cost <- function(baseline_settings, intervention_settings, intervention) {
-  population <- baseline_settings$population
-  procurement_buffer <- (intervention_settings$procureBuffer + 100) / 100
-  cost_per_net_llin <- intervention_settings$priceNetStandard
-  cost_per_net_pbo <- intervention_settings$priceNetPBO
-  cost_per_net_pyrrole <- intervention_settings$priceNetPyrrole
-  cost_delivery_per_net <- intervention_settings$priceDelivery
-  people_per_net <- intervention_settings$procurePeoplePerNet
-  cost_irs_per_person <- intervention_settings$priceIRSPerPerson
-  switch(
-    intervention,
-    "none" = 0,
-    "llin" = (cost_delivery_per_net + cost_per_net_llin) * (population / people_per_net * procurement_buffer),
-    "llin-pbo" = (cost_delivery_per_net + cost_per_net_pbo) * (population / people_per_net * procurement_buffer),
-    "pyrrole-pbo" = (cost_delivery_per_net + cost_per_net_pyrrole) * (population / people_per_net * procurement_buffer),
-    "irs" = 3 * cost_irs_per_person * population,
-    "irs-llin" = (cost_delivery_per_net + cost_per_net_llin) * (population / people_per_net * procurement_buffer) + 3 * cost_irs_per_person * population,
-    "irs-llin-pbo" = (cost_delivery_per_net + cost_per_net_pbo) * (population / people_per_net * procurement_buffer) + 3 * cost_irs_per_person * population,
-    "irs-pyrrole-pbo" = (cost_delivery_per_net + cost_per_net_pyrrole) * (population / people_per_net * procurement_buffer) + 3 * cost_irs_per_person * population
-  )
-}
+strategise <- function(options) {
+  combined_region_data <- combine_region_data(options$regions)
+  cost_thresholds <- c(1, 0.95, 0.9, 0.85, 0.8)
+  
+  lapply(cost_thresholds, function(threshold) {
+    budget <- options$budget * threshold
+    interventions <- do_optimise(combined_region_data, budget)
 
-get_intervention_data <- function(baseline_settings, intervention_settings, db) {
-  table <- db$get_table(baseline_settings)
-  rows <- table$intervention == "none"
-  if (intervention_settings$netUse != "0") {
-    rows <- rows | (
-      table$intervention %in% c("llin", "llin-pbo", "pyrrole-pbo") &
-      table$netUse == intervention_settings$netUse &
-      table$irsUse == "n/a"
-    )
-  }
-  if (intervention_settings$irsUse != "0") {
-    rows <- rows | (
-      table$intervention == "irs" &
-      table$netUse == "n/a" &
-      table$irsUse == intervention_settings$irsUse
-    )
-  }
-  if (intervention_settings$netUse != "0"
-    && intervention_settings$irsUse != "0") {
-    rows <- rows | (
-      table$intervention %in% c("irs-llin", "irs-llin-pbo", "irs-pyrrole-pbo") &
-      table$netUse == intervention_settings$netUse &
-      table$irsUse == intervention_settings$irsUse
-    )
-  }
-  table <- table[rows, c("intervention", "casesAverted", "casesAvertedErrorMinus", "casesAvertedErrorPlus")]
-  colnames(table) <- c("intervention", "cases_averted", "cases_averted_error_minus", "cases_averted_error_plus")
-  # costs are calculated across 3 years in `get_cost` so do the same for cases averted here
-  table$cases_averted <- table$cases_averted * 3
-  table$cases_averted_error_minus <- table$cases_averted_error_minus * 3
-  table$cases_averted_error_plus <- table$cases_averted_error_plus * 3
-  table
-}
-
-get_cost_data <- function(baseline_settings, intervention_settings, interventions) {
-  costs <- lapply(interventions, function(intervention) {
-    cost <- get_cost(baseline_settings, intervention_settings, intervention)
-    list(intervention = intervention, cost = cost)
-  })
-  do.call(rbind.data.frame, costs)
-}
-
-strategise <- function(options, db) {
-  data <- lapply(options$zones, function(zone) {
-    data <- get_intervention_data(zone$baselineSettings,
-                                  zone$interventionSettings, db)
-    cost <- get_cost_data(zone$baselineSettings, zone$interventionSettings,
-                      unique(data$intervention))
-    data <- merge(data, cost, by = "intervention", sort = FALSE)
-    data$zone <- zone$name
-    data
-  })
-  data <- do.call(rbind, data)
-  # The optimiser doesn't preserve extraneous columns so we save them here
-  # and merge them back into its output
-  errors <- data[c("zone", "intervention", "cases_averted_error_minus", "cases_averted_error_plus")]
-  lapply(c(1, 0.95, 0.9, 0.85, 0.8), function(cost_threshold) {
-    budget <- options$budget * cost_threshold
-    res <- do_optimise(data, budget)
-    res <- merge(res, errors, by = c("zone", "intervention"), sort = FALSE)
-    names(res)[names(res) == "cases_averted"] <- "casesAverted"
-    names(res)[names(res) == "cases_averted_error_minus"] <- "casesAvertedErrorMinus"
-    names(res)[names(res) == "cases_averted_error_plus"] <- "casesAvertedErrorPlus"
     list(
-      costThreshold = cost_threshold,
-      interventions = res[c("zone", "intervention", "cost", "casesAverted", "casesAvertedErrorMinus", "casesAvertedErrorPlus")]
+      costThreshold = threshold,
+      interventions = interventions
     )
   })
+}
+
+combine_region_data <- function(regions_df) {  
+  regions_df |>
+    # add no_intervention to each region's interventions
+    dplyr::mutate(
+      interventions = purrr::map(interventions, add_no_intervention)
+    ) |>
+    tidyr::unnest(interventions)
+}
+
+add_no_intervention <- function(interventions_df) {
+  no_intervention <- data.frame(
+    intervention = "no_intervention", 
+    cost = 0, 
+    casesAverted = 0
+  )
+  dplyr::bind_rows(interventions_df, no_intervention)
 }
--- tests/testthat/helper-mintr.R
+++ tests/testthat/helper-mintr.R
@@ -11,3 +11,27 @@ mintr_test_db_init <- function() {
     mintr_db_download("data")
   }
 }
+
+create_strategise_test_data <- function() {
+  json_txt <- '{
+  "budget": 10000,
+  "regions": [
+    {"region": "Region A", "interventions": [
+      {"intervention": "lsm_only", "cost": 1000, "casesAverted": 50}
+    ]},
+    {"region": "Region B", "interventions": [
+      {"intervention": "irs_only", "cost": 1500, "casesAverted": 70},
+      {"intervention": "lsm_only", "cost": 2500, "casesAverted": 150},
+      {"intervention": "py_only", "cost": 3000, "casesAverted": 200}
+    ]},
+    {"region": "Region C", "interventions": [
+      {"intervention": "irs_only", "cost": 2000, "casesAverted": 80},
+      {"intervention": "lsm_only", "cost": 3000, "casesAverted": 160},
+      {"intervention": "py_only", "cost": 3500, "casesAverted": 220},
+      {"intervention": "py_pbo_with_lsm", "cost": 4500, "casesAverted": 320}
+    ]}
+  ]
+}'
+  jsonlite::fromJSON(json_txt, flatten = TRUE, simplifyVector = TRUE)
+}
+
